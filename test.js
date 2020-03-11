455cc2516223e1f7ca7b74dbd7613a3a87f80320
hirsch <gery.hirschfeld@w3tec.ch>
2020-03-11 10:51:19 +0100

    Initial commit

---
+++ test.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const Log = require('./lib/log')
+const GateClient = require('./lib/gate-client');
+
+(async function () {
+    Log.info('START To open the gate')
+    const gateClient = GateClient(process.env.PLC_IP)
+    await gateClient.connect()
+    await gateClient.open()
+    gateClient.disconnect()
+    Log.info('EXIT Finished opening gate request')
+})()
