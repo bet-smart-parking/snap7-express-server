c4f2f04a555f991217ef2fdb20a4de6386bb7d15
Martin Kaiser <lists@martin-kaiser.info>
2020-05-19 11:26:57 +0200

    add config reader

--- logo-mock/logo-mock.py
+++ logo-mock/logo-mock.py
@@ -4,19 +4,21 @@ import time
 import snap7
 from snap7 import util
 import logging
+import configparser
 
-
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
 server = snap7.server.Server()
 server.create()
-
 size = 1
 data = (snap7.types.wordlen_to_ctypes[snap7.types.S7WLByte] * size)()
 data[0] = 5
 server.register_area(snap7.types.srvAreaDB, 1, data)
-
 server.start()
+
+config = configparser.ConfigParser()
+config.read('logo-mock.ini')
+logging.info("Slackbot URL: " + config['SLACK']['SlackBotUrl'])
 
 try:
     while (True):
